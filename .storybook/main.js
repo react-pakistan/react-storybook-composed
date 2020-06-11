4e4e772436d6e47f9fdce7d7efdfb7d14c7b3a48
Taimoor Khan <taimoor.muhammad@arbisoft.com>
2020-06-11 08:43:29 +0500

    :recycle: added multiple storybooks examples

---
+++ .storybook/main.js
@@ -0,0 +1,20 @@
+module.exports = {
+  refs: {
+    reactCommon: {
+      title: 'React Commons Collection',
+      url: 'https://taimoormk.github.io/react-commons-collection',
+    },
+    reactUI: {
+      title: 'React UI Collection',
+      url: 'https://taimoormk.github.io/react-ui-collection',
+    },
+    reactIcon: {
+      title: 'React Icon Collection',
+      url: 'https://taimoormk.github.io/react-icon-collection',
+    },
+    reactLogo: {
+      title: 'React Logo Collection',
+      url: 'https://taimoormk.github.io/react-logo-collection/?path=/story/logos--bitcoincolorlogos',
+    },
+  },
+};
