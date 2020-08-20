c96fdbbd6e64a8d07b844006419480fa3424d8f1
Taimoor Khan <taimoor.muhammad@arbisoft.com>
2020-08-20 13:43:28 +0500

    :recycle: hooked common library

--- src/a/props.ts
+++
@@ -1,8 +0,0 @@
-import { text } from '@storybook/addon-knobs';
-import { IAProps, theme } from '..';
-
-export const aProps = () : IAProps => ({
-  color: text('color', theme.colors.black),
-  href: text('href', 'https://google.com'),
-  target: text('target', '_blank'),
-});
