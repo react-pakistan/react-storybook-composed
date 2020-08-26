375a6036982bcb803b014690ac52cd193dad7a3a
Taimoor Khan <taimoor.muhammad@arbisoft.com>
2020-08-26 20:55:58 +0500

    :recycle: minor amends

--- .storybook/custom-theme.js
+++ .storybook/custom-theme.js
@@ -1,8 +1,10 @@
-import { create } from "@storybook/theming";
+import { create } from '@storybook/theming';
+import { reactPakistanTheme } from '@taimoormk/util-functions';
 
 export default create({
-  base: "light",
-  brandTitle: "React Storybook",
-  brandUrl: "https://github.com/react-pakistan/react-storybook",
-  brandImage: "https://res.cloudinary.com/dq6hflqwx/image/upload/v1565548545/GitHub/React_Pakistan_Storybook_Logo.jpg",
+  ...reactPakistanTheme,
+
+  brandTitle: 'React Storybook',
+  brandUrl: 'https://github.com/react-pakistan/react-storybook',
+  brandImage: 'https://res.cloudinary.com/dq6hflqwx/image/upload/v1565548545/GitHub/React_Pakistan_Storybook_Logo.jpg',
 });
